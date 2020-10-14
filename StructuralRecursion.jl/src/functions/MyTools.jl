73b68c698295980b3ac40ded3f74b261be5d41b8
jackoconnor <jackoconnor007@gmail.com>
2020-10-14 16:31:44 +0100

    Question 2 completed

--- StructuralRecursion.jl/src/functions/MyTools.jl
+++ StructuralRecursion.jl/src/functions/MyTools.jl
@@ -39,6 +39,19 @@ function intervalmembership(list::LinkedList, x::Float64)
             return list.data
         end
     end
-    
+
     return Nothing()
+end
+
+function intervalmembership(T::Union{FenwickTree, Nothing}, x::Float64)
+    #if T is a leaf we return it
+    if T.data.key != -1
+        return T.data
+    else
+        if x <= T.left.data.value
+            intervalmembership(T.left, x)
+        else
+            intervalmembership(T.right, x-T.left.data.value)
+        end
+    end
 end
